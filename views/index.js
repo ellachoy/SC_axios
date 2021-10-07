97be233d2c9b7fba1d73426661b92a58c74232a5
Ella Choy <egal21de@gmail.com>
2021-10-07 09:34:52 +0200

    add process port and script start

--- views/index.js
+++ views/index.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express()
-const port = 3000
+//
+const port = process.env.PORT || 3000
 
-app.set('views', './views')
+// app.set('views', './views')
+//app.set('view engine','ejs')
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
 })
 
 app.get('/', (req, res) => {
-  res.render('index.ejs',{title})
+  res.render('index.ejs', { title: 'home' })
 })
 app.get('/about', (req, res) => {
   res.render('about.ejs')
